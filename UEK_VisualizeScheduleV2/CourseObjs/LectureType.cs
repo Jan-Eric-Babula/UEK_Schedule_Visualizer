34f9c7743a2a779824de4801ace5ecb7bb973777
Jan Eric Babula <jan.eric.babula@gmail.com>
2019-05-29 15:42:27 +0200

    Added documentation to LectureType.cs

--- UEK_VisualizeScheduleV2/CourseObjs/LectureType.cs
+++ UEK_VisualizeScheduleV2/CourseObjs/LectureType.cs
@@ -7,13 +7,33 @@ using System.Threading.Tasks;
 namespace UEK_VisualizeScheduleV2
 {
     //TODO Add hash creation override
+    /// <summary>
+    /// Class to represent the possible types of lectures.
+    /// </summary>
     class LectureType
     {
-        public string Name { get; }
-        public string RawName { get; }
-        public string RawNameL { get; }
-        public string Hash { get; }
+        #region Properties
 
+        ///<value><c>Name</c> is the display name of the lecture type.</value>
+        public string Name { get; }
+
+        ///<value><c>RawName</c> is the given/polish designation of the lecture type.</value>
+        public string RawName { get; }
+
+        ///<value><c>RawNameL</c> is the given/polish designation of the lecture type with lowercase letters.</value>
+        public string RawNameL { get; }
+
+        ///<value><c>Hash</c> is the individual hash of the instance.</value>
+        public string Hash { get; }
+        
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates an instance of <c>LectureType</c>.
+        /// </summary>
+        /// <param name="rawname">The given/polish designation of the lecture type.</param>
         public LectureType (string rawname)
         {
             this.RawName = rawname.Trim();
@@ -22,6 +42,15 @@ namespace UEK_VisualizeScheduleV2
             this.Hash = Library.GetHashString(this.RawNameL);
         }
 
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Resolves and streamlines designations of given/polish lecture types.
+        /// </summary>
+        /// <param name="rawName">Given/polish designation of the lecture type.</param>
+        /// <returns>English and streamlined lecture type designations.</returns>
         private static string ResolveRawName(string rawName)
         {
             switch (rawName.Trim())
@@ -37,6 +66,14 @@ namespace UEK_VisualizeScheduleV2
             }
         }
 
+        #region Methods
+
+        /// <summary>
+        /// Evaluates if instance and a given object are equal.
+        /// Overrides original function to use internal <c>Hash</c> property.
+        /// </summary>
+        /// <param name="obj">Object to compare to.</param>
+        /// <returns>If the objects are equal.</returns>
         public override bool Equals(object obj)
         {
             if (obj.GetType() == this.GetType())
@@ -45,5 +82,9 @@ namespace UEK_VisualizeScheduleV2
             }
             return false;
         }
+
+        #endregion
+
+        #endregion
     }
 }
